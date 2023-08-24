60733b1bdda934ce8a4c0cd64bfe52abcfb2941e
emilykamiti <emilykamiti@gmail.com>
2023-08-25 00:14:18 +0300

    assignment operators

--- src/main/operators/operatorsdemo.java
+++ src/main/operators/operatorsdemo.java
@@ -36,6 +36,11 @@ public class OperatorsDemo {
         LOGGER.info("Remainder: " + remainder);
 
     }
+    public static void main(String[] args) {
+        OperatorsDemo app = new OperatorsDemo();
+        // app.arithmeticOperators();
+        app.comparisonOperators();
+    }
 
     public void assignmentOperators() {
         //simple assignment operator
@@ -52,40 +57,6 @@ public class OperatorsDemo {
         quantity /= 20; // > quantity = quantity / 20;
         LOGGER.info("Quantity: " + quantity);
     }
-
-    public void comparisonOperators() {
-        int studentOneAge = 20;
-        int studentTwoAge = 25;
-
-        // equality operator
-        boolean isStudentOneAgeEqualToStudentTwoAge = studentOneAge == studentTwoAge;
-        LOGGER.info("isStudentOneAgeEqualToStudentTwoAge: " + isStudentOneAgeEqualToStudentTwoAge);
-
-        // Inequality operator
-        boolean isStudentOneAgeNotEqualToStudentTwoAge = studentOneAge != studentTwoAge;
-        LOGGER.info("isStudentOneAgeNotEqualToStudentTwoAge: " + isStudentOneAgeNotEqualToStudentTwoAge);
-
-        // less than operator
-        boolean isStudentOneAgeLessThanStudentTwoAge = studentOneAge < studentTwoAge;
-        LOGGER.info("isStudentOneAgeLessThanStudentTwoAge: " + isStudentOneAgeLessThanStudentTwoAge);
-
-        // greater than operator
-        boolean isStudentOneAgeGreaterThanStudentTwoAge = studentOneAge > studentTwoAge;
-        LOGGER.info("isStudentOneAgeGreaterThanStudentTwoAge: " + isStudentOneAgeGreaterThanStudentTwoAge);
-
-        // TODO: Do one for less than or equal to operator
-        //code here
-        boolean isStudentOneAgeLessThanOrEqualToStudentTwoAge = studentOneAge <= studentTwoAge;
-        LOGGER.info("isStudentOneAgeLessThanOrEqualToStudentTwoAge: " + isStudentOneAgeLessThanOrEqualToStudentTwoAge);
-
-
-        // TODO: Do one for greater than or equal to operator
-        //code here
-        boolean isStudentOneAgeGeaterThanOrEqualToStudentTwoAge = studentOneAge >= studentTwoAge;
-        LOGGER.info("isStudentOneAgeGreaterThanOrEqualToStudentTwoAge: " + isStudentOneAgeGreaterThanOrEqualToStudentTwoAge);
-
-    }
-
     public static void main(String[] args) {
         OperatorsDemo app = new OperatorsDemo();
         // app.arithmeticOperators();
@@ -93,3 +64,5 @@ public class OperatorsDemo {
         app.comparisonOperators();
     }
 }
+
+    
