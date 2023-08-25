b17e14137698ce2e50ae73b680c0c55d50cee92a
emilykamiti <emilykamiti@gmail.com>
2023-08-25 07:49:47 +0300

    build

---
+++ src/main/java/com/systechafrica/Math.java
@@ -0,0 +1,7 @@
+package com.systechafrica;
+
+public class Math {
+    public int subtract(int a, int b){
+    return a - b;
+    }
+}
---
+++ src/test/java/com/systechafrica/MathTest.java
@@ -0,0 +1,13 @@
+package com.systechafrica;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class MathTest {
+     @Test
+        void test() {
+            System.out.println("This test run");
+
+        }
+
+     }
+
