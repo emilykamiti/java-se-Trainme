60fddd7880eda59bf3e0ade1beecbe2a91ec8faf
emilykamiti <emilykamiti@gmail.com>
2023-08-28 01:33:52 +0300

    Switchstatemennts

--- src/main/java/com/systechafrica/ControlF/Cflow.java
+++ src/main/java/com/systechafrica/ControlF/Cflow.java
@@ -25,7 +25,30 @@ public static final Logger LOGGER = Logger.getLogger(Cflow.class.getSimpleName()
      LOGGER.info("Student grade: " + grade); 
 }
     public void switchStatement(){
-
+        String day = "Unknown";
+        switch(day){
+            case "MONDAY":
+                LOGGER.info( "Monday is a working day");
+                break;  
+            case "TUESDAY":
+                LOGGER.info( "Tuesday is a working day");
+                break;
+            case "WEDNESDAY":
+                LOGGER.info( "Wednesday is a working day");
+                break;
+            case "THURSDAY":
+                LOGGER.info( "Thursday is a working day");
+                break;
+            case "FRIDAY":
+                LOGGER.info( "Friday is a working day");
+                break;
+            case "SATURDAY":
+            case "SUNDAY":
+                LOGGER.info("Party");
+                break;
+            default:
+                LOGGER.info("Please specify a valid day");
+        }
     }
     
 
