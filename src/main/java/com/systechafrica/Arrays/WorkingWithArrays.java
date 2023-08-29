578adde7d439aeb5f9a33b8b87e7dda27948813d
emilykamiti <emilykamiti@gmail.com>
2023-08-29 20:48:26 +0300

    Arrays

---
+++ src/main/java/com/systechafrica/Arrays/WorkingWithArrays.java
@@ -0,0 +1,8 @@
+package com.systechafrica.Arrays;
+
+import java.util.logging.Logger;
+
+public class WorkingwithArrays {
+    
+     private static final Logger LOGGER = Logger.getLogger(WorkingwithArrays.class.getName());
+}
