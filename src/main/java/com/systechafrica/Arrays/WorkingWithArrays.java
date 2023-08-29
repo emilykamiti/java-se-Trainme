11473e28af99eba6f67f27d496522f58837bb67b
emilykamiti <emilykamiti@gmail.com>
2023-08-29 21:32:46 +0300

    Working with Arrays

--- src/main/java/com/systechafrica/Arrays/WorkingWithArrays.java
+++ src/main/java/com/systechafrica/Arrays/WorkingWithArrays.java
@@ -2,7 +2,27 @@ package com.systechafrica.Arrays;
 
 import java.util.logging.Logger;
 
-public class WorkingwithArrays {
+public class WorkingWithArrays {
     
-     private static final Logger LOGGER = Logger.getLogger(WorkingwithArrays.class.getName());
+     private static final Logger LOGGER = Logger.getLogger(WorkingWithArrays.class.getName());
+
+     public void singleDimensionalArray(){
+        int[] numbers = {45, 10, 26, 35, 40, 50 };
+        
+        String[] nickName = {"Emily", "Joy", "Nasinza"};
+
+        for (int i = 0; i < numbers.length; i++){
+            LOGGER.info(""+numbers[i]);
+
+        }
+        LOGGER.info(nickName[0]);
+        LOGGER.info(nickName[1]); 
+        LOGGER.info(nickName[2]);
+        
+
+     }
+      public static void main(String[] args) {
+        WorkingWithArrays app = new WorkingWithArrays();
+        app.singleDimensionalArray();
+     }
 }
