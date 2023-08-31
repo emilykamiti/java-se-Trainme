4a16eb392ca5b423eeb5838fa755351f510bdc14
emilykamiti <emilykamiti@gmail.com>
2023-08-31 12:47:09 +0300

    atmMachine

--- src/main/java/com/systechafrica/UserInput/atmMachime.java
+++ src/main/java/com/systechafrica/UserInput/atmMachime.java
@@ -1,45 +1,42 @@
 package com.systechafrica.UserInput;
 
+
 import java.util.Scanner;
 
-public class atmMachime {
-
+class atmMachine {
     public static void main(String[] args) {
+     
+    final String User_name = "Emily";
+    final String User_password = "Kamiti101";
 
+    int Attempts = 0;
+    
+    double accountBalance = 1000;
+        Scanner Ear = new Scanner(System.in);
 
-        final String User_Name = "Kamiti";
-        final String user_Password = "Admin123";
-        int loginTrial = 0;
-        double accountBalance = 1000;
-        Scanner scanner = new Scanner(System.in);
-
-        while (loginTrial <= 3) {
-            System.out.println("Input password");
-
-            System.out.print("Username: ");
-            String username = scanner.nextLine();
-            System.out.print("Password: ");
-            String userPassword = scanner.nextLine();
-
-            if (username.equals(User_Name) && userPassword.equals(user_Password)) {
-                System.out.println("Login successful");
+        while(Attempts < 3){
+            System.out.println("Tell the Ear your secret:");
+            
+            System.out.print("Your name: ");
+            String nameInput = Ear.nextLine();
+            System.out.print("Your Password: ");
+            String passInput = Ear.nextLine();
+            Ear.nextLine();
+            
+            if (nameInput.equals(User_name) && passInput.equals(User_password )){
+                
+                System.out.println("SUccessful");
                 break;
             } else {
-                System.out.println("Login was unsuccessful. Please try again.");
-                loginTrial++;
-            }
-            if (loginTrial == 3) {
-                System.err.println("You've reached Maximum login attemps");
-                scanner.close();
-                    return;
+                System.out.println("Aaaah Not right, Try again!");
+                Attempts++;
                 
             }
-        }
-
-        if (loginTrial == 3) {
-            System.err.println("You have exceeded the maximum login attempts. The system will now stop.");
-            scanner.close();
-            return;
+            
+            if (Attempts == 3){
+                System.err.println("You number of attempts is over.");
+                Ear.close();
+                return;
         }
 
         while (true) {
@@ -56,21 +53,21 @@ public class atmMachime {
             System.out.println("\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"");
 
             System.out.println("Please choose an option");
-            int option_chosen = scanner.nextInt();
+            int chose_option = Ear.nextInt();
 
-            switch (option_chosen) {
+            switch (chose_option) {
                 case 1:
                     System.out.println("Your account balance is ksh. " + accountBalance);
                     break;
                 case 2:
                     System.out.print("Enter deposit  ");
-                    double amountToDeposit = scanner.nextDouble();
+                    double amountToDeposit = Ear.nextDouble();
                     accountBalance += amountToDeposit;
                     System.out.print("Your new account balance is ksh. " + accountBalance);
                     break;
                 case 3:
                     System.out.print("Enter the amount you want to withdraw: ");
-                    double amountWithdraw = scanner.nextDouble();
+                    double amountWithdraw = Ear.nextDouble();
                     double ChargesForWithdraw = amountWithdraw * 0.02;
                     
                     if (amountWithdraw + ChargesForWithdraw < accountBalance) {
@@ -85,9 +82,14 @@ public class atmMachime {
                     System.out.print("Service not wworking at a moment");
                     break;
                 case 5:
-                    scanner.close();
+                    Ear.close();
+                    return;
+                default:
+                    System.out.print("Please enter valid input");
+                    break;
             }
         }
     }
 
+    }
 }
