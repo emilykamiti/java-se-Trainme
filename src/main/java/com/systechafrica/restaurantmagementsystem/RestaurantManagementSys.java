695ad58e55079cac18a8d2412316f449bb463923
emilykamiti <emilykamiti@gmail.com>
2023-09-11 00:23:19 +0300

    Added menu options

--- src/main/java/com/systechafrica/restaurantmagementsystem/RestaurantManagementSys.java
+++ src/main/java/com/systechafrica/restaurantmagementsystem/RestaurantManagementSys.java
@@ -8,16 +8,66 @@ public class RestaurantManagementSys {
     Scanner scanner = new Scanner(System.in);
     private final String USER_PASSWORD = "Admin123";
     private List<Drinks> drinksMenu = new ArrayList<>();
-    private List<Meals> MealsMenu = new ArrayList<>();
-
+    private List<Meals> mealsMenu = new ArrayList<>();
 
     public static void main(String[] args) {
         RestaurantManagementSys restaurantms = new RestaurantManagementSys();
         boolean loggedIn = restaurantms.login();
 
-      
+        if (loggedIn) {
+            System.out.println("Welcome to Sytech restaurant!");
+
+            boolean showingOutput = true;
+            while (showingOutput) {
+                restaurantms.displayMenu();
+                int option = restaurantms.scanner.nextInt();
+                restaurantms.scanner.nextLine();
+
+                if (option == 1) {
+                    restaurantms.addItem("Chai", 15);
+
+                } else if (option == 2) {
+                    restaurantms.addItem("Andazi", 10);
+
+                } else if (option == 3) {
+                    restaurantms.addItem("Tosti", 12);
+
+                } else if (option == 4) {
+                    restaurantms.addItem("Ndegu And Accompaniments", 70);
+
+                } else if (option == 5) {
+                    restaurantms.addItem("Beans And Accompaniments", 70);
+
+                } else if (option == 6) {
+                    restaurantms.addItem("Pilau Veg", 90);
+
+                } else if (option == 7) {
+                    showingOutput = false;
+
+                } else {
+                    System.out.println("Invalid input");
+                }
+
+            }
+
+            if (restaurantms.mealsMenu.isEmpty() || !restaurantms.drinksMenu.isEmpty()) {
+                System.out.println("Continue to payment");
+                String continuetoPayment = restaurantms.scanner.nextLine();
+                if (continuetoPayment.equalsIgnoreCase("Y")) {
+                    restaurantms.displayPayment();
+                } else {
+                    System.out.println("Cannot continue with payment");
+                }
+            } else {
+                System.out.println("No items picked");
+            }
+        } else {
+            System.out.println("Maximum Login attempts reached, try again");
+        }
     }
 
+    private void addItem(String string, int i) {
+    }
 
     public boolean login() {
         int loginAttempts = 1;
@@ -38,19 +88,20 @@ public class RestaurantManagementSys {
 
     }
 
-    public void MealsandDrinksmenu(){
+    public void MealsandDrinksmenu() {
         drinksMenu.add(new Drinks("CHAI", 15.0));
         drinksMenu.add(new Drinks("ANDAZI", 15.0));
         drinksMenu.add(new Drinks("TOSTI", 15.0));
-        MealsMenu.add(new Meals("NDEGU AND ACCOMPANIMENTS", 70.0));
-        MealsMenu.add(new Meals("BEANS AND ACCOMPANIMENTS", 70.0));
-        MealsMenu.add(new Meals("PILAU VEG", 90.0));
-    
+        mealsMenu.add(new Meals("NDEGU AND ACCOMPANIMENTS", 70.0));
+        mealsMenu.add(new Meals("BEANS AND ACCOMPANIMENTS", 70.0));
+        mealsMenu.add(new Meals("PILAU VEG", 90.0));
 
     }
-    public void Ordering(){
-        
+
+    public void orderedItems() {
+
     }
+
     public void displayMenu() {
         System.out.println("********************");
         System.out.println(" SYSTECH RESTAURANT");
@@ -70,5 +121,8 @@ public class RestaurantManagementSys {
 
     }
 
+    public void displayPayment() {
+
+    }
 
 }
