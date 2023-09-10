dc5e71a17fefa4accaea2de238ef4a7353236a44
emilykamiti <emilykamiti@gmail.com>
2023-09-10 10:54:14 +0300

    Loggin method for a restauran management sys

--- src/main/java/com/systechafrica/restaurantmagementsystem/RestaurantManagementSys.java
+++ src/main/java/com/systechafrica/restaurantmagementsystem/RestaurantManagementSys.java
@@ -8,27 +8,24 @@ public class RestaurantManagementSys {
 
     public static void main(String[] args) {
 
-
     }
 
-        public boolean login() {
-            int loginAttempts = 1;
-            boolean loggedIn = false;
-    
-            while (loginAttempts <= 3) {
-                System.out.println("Enter your password: ");
-                String userPassword = scanner.nextLine();
-                if (userPassword.equals(USER_PASSWORD)) {
-                    loggedIn = true;
-                    break;
-                }
-                System.out.println("Wrong password");
-                loginAttempts++; 
-    
+    public boolean login() {
+        int loginAttempts = 1;
+        boolean loggedIn = false;
+
+        while (loginAttempts <= 3) {
+            System.out.println("Enter your password: ");
+            String userPassword = scanner.nextLine();
+            if (userPassword.equals(USER_PASSWORD)) {
+                loggedIn = true;
+                break;
             }
-            return loggedIn;
-        }
+            System.out.println("Wrong password");
+            loginAttempts++;
 
+        }
+        return loggedIn;
     }
 
-
+}
