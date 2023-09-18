fe1620f79e237e71dfd1b1e327dd3357b2a676b3
emilykamiti <emilykamiti@gmail.com>
2023-09-18 12:01:55 +0300

    Static classes and static methods

--- src/main/java/com/systechafrica/Part2/staticclasses/Mathutils.java
+++ src/main/java/com/systechafrica/Part2/staticclasses/Mathutils.java
@@ -1,7 +1,39 @@
 package com.systechafrica.Part2.staticclasses;
 
-public class Mathutils {
-    // private static clss Calculator{
-    //     public static int add
+public class MathUtils {
+    private String operationName;
+    public void printInfo(){
+        System.out.println("Printing");
+
+    }
+    public static class Calculator {
+        public static int add(int a, int b) {
+            MathUtils math = new MathUtils();
+            System.out.println(math.operationName); //! that to access the on-static member,
+                                                    //! you need an object of the enclossing class
+          math.printInfo();
+         return a + b;
+        } 
+        
+        public static long add(long a, long b) {
+    return a + b;
+        }
+         public static long add(long a, long b, long c) {
+    return a + b + c;
+        }
+        
+        
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+    }
+    //public static sum(int a, int b){
+    //     return Calculator.add(a, b);
     // }
+    public static class Area extends Calculator{
+        public static int square(int a, int b){
+            return multiply(a, b);
+        }
+    }
 }
