285b13959066847e5d294dc18c7e4f91c3833601
emilykamiti <emilykamiti@gmail.com>
2023-10-07 09:13:23 +0300

    Commented test file

--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -1,22 +1,22 @@
-package com.systechafrica;
+// package com.systechafrica;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-@TestInstance(TestInstance.Lifecycle.PER_CLASS) //? default insance
-public class AppTest {
-    //? Reference => App
-    App app = new App();
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.TestInstance;
+// @TestInstance(TestInstance.Lifecycle.PER_CLASS) //? default insance
+// public class AppTest {
+//     //? Reference => App
+//     App app = new App();
 
 
-    @Test
-    void add(){
-        //when
-        int result = app.add(2, 3);
-        //then
-        int expected = 5;
-        //verify that result = expected
-        Assertions.assertEquals(expected, result, "The sum of 2 + 5 should be 5");
-    }
+//     @Test
+//     void add(){
+//         //when
+//         int result = app.add(2, 3);
+//         //then
+//         int expected = 5;
+//         //verify that result = expected
+//         Assertions.assertEquals(expected, result, "The sum of 2 + 5 should be 5");
+//     }
 
-}
+// }
--- src/test/java/com/systechafrica/interfaces/CalculatorTest.java
+++ src/test/java/com/systechafrica/interfaces/CalculatorTest.java
@@ -1,56 +1,56 @@
-package com.systechafrica.interfaces;
+// package com.systechafrica.interfaces;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
 
-import com.systechafrica.part2.interfaces.Calculator;
-
-
-
-public class CalculatorTest {
-    Calculator calculator;
-
-    @BeforeEach
-    void setup(){
-        calculator = new CalculatorImpl(); // correct calculator implementation
-    }
+// import com.systechafrica.part2.interfaces.Calculator;
 
 
-    @Test
-    void summations(){
-        // inputs
-        int a = 10;
-        int b = 20;
 
-        //expectated results
-        int expectedResult = 30;
+// public class CalculatorTest {
+//     Calculator calculator;
 
-        // invoke the requests to the functio
-        int actualResult = calculator.sum(a, b);
-
-        Assertions.assertEquals(expectedResult, actualResult);
-
-        //verify results
-
-    }
+//     @BeforeEach
+//     void setup(){
+//         calculator = new CalculatorImpl(); // correct calculator implementation
+//     }
 
 
-    class CalculatorImpl implements Calculator{
+//     @Test
+//     void summations(){
+//         // inputs
+//         int a = 10;
+//         int b = 20;
 
-        @Override
-        public int sum(int a, int b) {
-            return a + b;
-        }
+//         //expectated results
+//         int expectedResult = 30;
 
-        @Override
-        public int sum(int a, int b, int... otherValues) {
-            int sum = a+b;
-            for (int number: otherValues){
-                sum = sum + number;
-            }
-            return sum;
-        }
+//         // invoke the requests to the functio
+//         int actualResult = calculator.sum(a, b);
 
-    }
-}
+//         Assertions.assertEquals(expectedResult, actualResult);
+
+//         //verify results
+
+//     }
+
+
+//     class CalculatorImpl implements Calculator{
+
+//         @Override
+//         public int sum(int a, int b) {
+//             return a + b;
+//         }
+
+//         @Override
+//         public int sum(int a, int b, int... otherValues) {
+//             int sum = a+b;
+//             for (int number: otherValues){
+//                 sum = sum + number;
+//             }
+//             return sum;
+//         }
+
+//     }
+// }
